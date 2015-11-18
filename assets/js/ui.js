dbe0b2fac4f3ac2a5142c23b827a8cf366eaa881
emanuelfeld <emanuelfeld@gmail.com>
2015-11-18 13:21:41 -0500

    Remove _site folder

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
+  function searchGlossary (search, by) {
+    $(glossary_data).each(function(index, value) {
+      if (value[by].toLowerCase().indexOf(search) < 0) {
+        $("#" + value.slug).attr("hidden-term", "true");
+      } else {
+        $("#" + value.slug).attr("hidden-term", "false");       
+      }
+    });
+  }
+
+  function findFirst (data, by, match) {
+    for (var i = 0, len = data.length; i < len; i++) {
+      if (data[i][by] === match)
+          return data[i];
+    }
+    return null;
+  }    
+
+  $(".term").click(function() {
+    var slug = $(this).attr("id");
+
+    var search_object = findFirst($(glossary_data), "slug", slug)
+    var search_string = search_object.term.toLowerCase();
+
+    searchGlossary(slug, "slug");
+
+    $("#glossary-search").val(search_string);
+    $("#glossary").toggle(display = true);
+  });
 
   $(".glossary-toggle").click(function() {
     $("#glossary").toggle();
@@ -7,20 +35,10 @@ $(document).ready(function() {
 
   $(".glossary-clear").click(function() {
     $("#glossary-search").val("");
-    // $(".glossary-term").attr("hidden-term", "false");
   });
 
   $("#glossary-search").on('input', function() {
-    var search = $("#glossary-search").val().toLowerCase();
-
-    $(glossary_data).each(function(index, value) {
-      if (value["term"].toLowerCase().indexOf(search) < 0) {
-        $("#" + value.slug).attr("hidden-term", "true")
-      } else {
-        $("#" + value.slug).attr("hidden-term", "false")        
-      }
-    });
+    var search_string = $("#glossary-search").val().toLowerCase();
+    searchGlossary(search_string, "term");
   });
-
-});
 
